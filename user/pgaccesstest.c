f778b344e468f1086dda6db342bd0f8b1f67ed58
mquangcao <mquangcao38@gmail.com>
2024-12-29 12:35:10 +0700

    modifier : modifier test

--- user/pgaccesstest.c
+++ user/pgaccesstest.c
@@ -1,35 +1,45 @@
+#include "kernel/param.h"
+#include "kernel/fcntl.h"
 #include "kernel/types.h"
-#include "kernel/stat.h"
+#include "kernel/riscv.h"
 #include "user/user.h"
 
-#define PGSIZE 4096 
+void pgaccess_test();
 
-int main() {
-    char *buffer = sbrk(2 * PGSIZE);
-    if (buffer == (char *)-1) {
-        printf("Failed to allocate memory\n");
-        exit(1);
-    }
+int
+main(int argc, char *argv[])
+{
+  pgaccess_test();
+  printf("pgtbltest: all tests succeeded\n");
+  exit(0);
+}
 
-    buffer[0] = 'x';
+char *testname = "???";
 
-    uint64 accessed = 0;
-    if (pgaccess(buffer, 2, (char *)&accessed) < 0) {
-        printf("pgaccess failed\n");
-        exit(1);
-    }
+void
+err(char *why)
+{
+  printf("pgtbltest: %s failed: %s, pid=%d\n", testname, why, getpid());
+  exit(1);
+}
 
-    if (accessed & 0x1) {
-        printf("Page 0 accessed\n");
-    } else {
-        printf("Page 0 not accessed\n");
-    }
-
-    if (accessed & 0x2) {
-        printf("Page 1 accessed\n");
-    } else {
-        printf("Page 1 not accessed\n");
-    }
-
-    exit(0);
+void
+pgaccess_test()
+{
+  char *buf;
+  unsigned int abits;
+  printf("pgaccess_test starting\n");
+  testname = "pgaccess_test";
+  buf = malloc(32 * PGSIZE);
+  if (pgaccess(buf, 32, &abits) < 0)
+    err("pgaccess failed");
+  buf[PGSIZE * 1] += 1;
+  buf[PGSIZE * 2] += 1;
+  buf[PGSIZE * 30] += 1;
+  if (pgaccess(buf, 32, &abits) < 0)
+    err("pgaccess failed");
+  if (abits != ((1 << 1) | (1 << 2) | (1 << 30)))
+    err("incorrect access bits set");
+  free(buf);
+  printf("pgaccess_test: OK\n");
 }
